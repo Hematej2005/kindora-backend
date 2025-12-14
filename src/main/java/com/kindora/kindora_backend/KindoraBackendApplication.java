3e84a5412b27fa188b7446ff45f1668690a78378
Hematej2005 <akkanavenkatahematej@gmail.com>
2025-12-15 01:25:14 +0530

    Update KindoraBackendApplication.java

--- src/main/java/com/kindora/kindora_backend/KindoraBackendApplication.java
+++ src/main/java/com/kindora/kindora_backend/KindoraBackendApplication.java
@@ -2,12 +2,15 @@ package com.kindora.kindora_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.kindora.kindora_backend.model")
+@EnableJpaRepositories(basePackages = "com.kindora.kindora_backend.repository")
 public class KindoraBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KindoraBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KindoraBackendApplication.class, args);
+    }
 }
