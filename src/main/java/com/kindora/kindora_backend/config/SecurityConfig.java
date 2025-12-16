49d119a9d21d59ac7d8f439ba1a9e8b3d3f36495
Hematej2005 <akkanavenkatahematej@gmail.com>
2025-12-16 20:00:25 +0530

    Update SecurityConfig.java

--- src/main/java/com/kindora/kindora_backend/config/SecurityConfig.java
+++ src/main/java/com/kindora/kindora_backend/config/SecurityConfig.java
@@ -44,9 +44,9 @@ public class SecurityConfig {
                         "/*.html",
                         "/*.css",
                         "/*.js",
-                        "/*.jpg"
-
-
+                        "/*.jpg",
+                        "/api/health"
+                    
                 ).permitAll()
 
                 // Everything else requires login
