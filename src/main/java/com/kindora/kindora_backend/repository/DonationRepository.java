cb64ef50ab5ca838412e8ce44a0ad8653300eaf9
Hematej2005 <akkanavenkatahematej@gmail.com>
2026-07-16 14:05:45 +0530

    Update DonationRepository.java

--- src/main/java/com/kindora/kindora_backend/repository/DonationRepository.java
+++ src/main/java/com/kindora/kindora_backend/repository/DonationRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
-
+    // used in the donar dashboard
     List<Donation> findByDonorIdOrderByCreatedAtDesc(Long donorId);
 
     // allow fetching donations by status
+    // used in the Nearbydonations
     List<Donation> findByStatus(String status);
+    List<Donation> findByStatusOrderByCreatedAtDesc(String status);
 
     @Query(value = "SELECT *,(6371 * acos(cos(radians(:lat)) * cos(radians(lat)) * " +
             "cos(radians(lng) - radians(:lng)) + sin(radians(:lat)) * sin(radians(lat)))) AS distance " +
@@ -23,8 +25,10 @@ public interface DonationRepository extends JpaRepository<Donation, Long> {
                               @Param("radius") double radius,
                               @Param("limit") int limit);
     // Add these two methods to the existing DonationRepository interface
+    //used in the member dashboard
+    // used to find assigned donation
     List<Donation> findByAssignedMemberIdOrderByAssignedAtDesc(Long memberId);
-
+    // used to find delivered donations
     List<Donation> findByAssignedMemberIdAndStatusOrderByAssignedAtDesc(Long memberId, String status);
 
 }
